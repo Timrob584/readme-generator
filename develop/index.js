731eb31bfec407d5a9547524077296ccaad829fa
Timrob584 <timrob584@gmail.com>
2021-10-05 23:02:08 -0400

    added function to initialize app

--- develop/index.js
+++ develop/index.js
@@ -5,42 +5,55 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
-        name: 'Title',
+        name: 'title',
         message: 'Enter project title here'
     },
     {
         type: 'input',
-        name: 'Description',
+        name: 'description',
         message: 'Provide a short description explaining the what, why, and how of your project.'
     },
     {
         type: 'input',
-        name: 'Installation',
+        name: 'installation',
         message: 'What are the steps required to install your project?'
     },
     {
         type: 'input',
-        name: 'Usage',
+        name: 'usage',
         message: 'Provide instructions and examples for use.'
-    },
-    {
-        type: 'input',
-        name: 'License',
-        message: 'Provide a license for your project if you have one.'
     },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (error) =>
-        error ? console.log(error) : console.log(data)
-    )
+    fs.writeFile(fileName, data, err =>
+        err ? console.log(err) : console.log(data)
+    );
 };
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(function(answers) {
-        
+    inquirer.prompt(questions).then(answers => {
+
+        const fileName = `./README.md`;
+        const content = 
+    `
+# ${answers.title}
+
+# Description
+${answers.description}
+
+# Installation 
+${answers.installation}
+
+
+# Usage 
+${answers.usage}
+
+    `
+    
+        writeToFile(fileName, content)
     })
 }
 
